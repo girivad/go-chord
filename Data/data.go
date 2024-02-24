22f984046114bfdd910c8fd3f851539d220134d5
Giridhar Vadhul <gvadhul@ucsc.edu>
2024-02-24 11:55:14 -0800

    Adopted and modified KV implementation from prior attempt at DHT in Go.

--- Data/data.go
+++ Data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -21,39 +22,92 @@ func NewDataServer() *DataServer {
 }
 
 func (dataServer *DataServer) GetValue(w http.ResponseWriter, r *http.Request) {
+	// Collect the key parameter.
 	key := mux.Vars(r)["key"]
 
-	if key == "" {
-		http.Error(w, http.StatusText(http.StatusBadRequest)+": Key not provided.", http.StatusBadRequest)
-		return
-	}
-
-	val, ok := dataServer.KVMap[key]
-
-	if !ok {
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
-	}
-	// Optimize, given that the marshalling is only for writing.
-	valueBytes, err := json.Marshal(val)
-
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError)+": Error encoding value for transmission.", http.StatusInternalServerError)
-		return
-	}
-
-	// Automatically sends Status OK Header.
-	w.Write(valueBytes)
-}
-
-func (dataServer *DataServer) PutValue(w http.ResponseWriter, r *http.Request) {
-	key := mux.Vars(r)["key"]
-
+	// Handle no key
 	if key == "" {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
+	// Retrieve value from map
+	value, found := dataServer.KVMap[key]
 
-	// Check if key is in the map => 201 if no, 200 if yes.
-	// val, ok := dataServer.KVMap[key]
+	// Handle not found
+	if !found {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	// Else, put in JSON and return
+
+	valBytes, err := json.Marshal(value)
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(valBytes)
+}
+
+func (dataServer *DataServer) PutValue(w http.ResponseWriter, r *http.Request) {
+	// Collect the key parameter.
+	key := mux.Vars(r)["key"]
+
+	if key == "" {
+		http.Error(w, http.StatusText(http.StatusBadRequest)+": No key provided.", http.StatusBadRequest)
+	}
+
+	// Retrieve Value from Body
+	requestBody, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	value := Value{}
+	err = json.Unmarshal(requestBody, &value)
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest)+": invalid or no value provided.", http.StatusBadRequest)
+		return
+	}
+
+	// Edit the key-value pair
+	_, found := dataServer.KVMap[key]
+	dataServer.KVMap[key] = value
+
+	var status int
+	if found {
+		status = http.StatusAccepted
+	} else {
+		status = http.StatusCreated
+	}
+
+	w.WriteHeader(status)
+	w.Write(([]byte)(http.StatusText(status)))
+}
+
+func (dataServer *DataServer) DeleteKV(w http.ResponseWriter, r *http.Request) {
+	key := mux.Vars(r)["key"]
+
+	if key == "" {
+		http.Error(w, http.StatusText(http.StatusBadRequest)+": No key provided.", http.StatusBadRequest)
+		return
+	}
+
+	_, found := dataServer.KVMap[key]
+
+	if !found {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	delete(dataServer.KVMap, key)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(([]byte)(http.StatusText(http.StatusOK)))
 }
