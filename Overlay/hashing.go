ca09b9556d9074b46a379c02a4c7b753c4c60b20
Giridhar Vadhul <gvadhul@ucsc.edu>
2024-03-21 17:56:52 -0700

    Always use 8 bytes of the hash to ensure sufficient bytes are provided for conversion to int64

--- Overlay/hashing.go
+++ Overlay/hashing.go
@@ -9,9 +9,13 @@ func hash(ip string, capacity int64) int64 {
 	// Placeholder Hash
 	// This has 160 bits.
 	hashBytes := sha1.Sum(([]byte)(ip))
-	relHashBytes := hashBytes[19-capacity/8:]
+	relHashBytes := hashBytes[8:]
 
-	return int64(binary.BigEndian.Uint64(relHashBytes)) % (1 << capacity)
+	relHashUint := binary.BigEndian.Uint64(relHashBytes)
+
+	relHashInt := int64(relHashUint)
+
+	return relHashInt % (1 << capacity)
 }
 
 func isBetween(candidate int64, start int64, end int64) bool {
